8bbaa39de0e062dcb124bb5b47a3c18d7638c82d
Bogdan Kovalev <mstbit@i.ua>
2015-04-10 13:16:44 +0300

    model test

--- test/js/model_test.js
+++ test/js/model_test.js
@@ -2,9 +2,8 @@ QUnit.test("ADD NEW TASK", function (assert) {
     var eventBus = {};
     var storage = new Storage("model-test-storage");
     var model = new Model(storage, eventBus);
-    var description = "Test description";
-    var user = "Test user";
-    var newTask = new TaskItem(description, user);
+
+    var newTask = new TaskItem("Test description", "Test user");
 
     $(eventBus).on(Event.MODEL_TASK_ADDED, function (event, data) {
         assert.deepEqual(data.task, newTask.getDTO(), "Passed!");
